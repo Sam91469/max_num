1486635d3035004592845fd6680feed7f7104607
Sam91469 <142690992+Sam91469@users.noreply.github.com>
2023-08-19 15:08:44 -0500

    Update stremlit_max_num.py

--- stremlit_max_num.py
+++ stremlit_max_num.py
@@ -9,4 +9,3 @@ number3 = st.number_input('Insert a third number')
 st.write('The current number is ', number3)
 st.write('The max number is:')
 st.write(max(number1,number2,number3))
-!streamlit run app.py & npx localtunnel --port 8501
