26e72c62255874831fdef31cfc050f154f3834dc
Sam91469 <142690992+Sam91469@users.noreply.github.com>
2023-08-19 14:58:08 -0500

    Create stremlit_max_num.py

---
+++ stremlit_max_num.py
@@ -0,0 +1,14 @@
+!pip install -q streamlit
+%%writefile app.py
+import streamlit as st
+st.write('# streamlit application for finding the largest among the 3 given numbers')
+st.write('#User Input Parameters')
+number1 = st.number_input('Insert a first number')
+st.write('The current number is ', number1)
+number2 = st.number_input('Insert a second number')
+st.write('The current number is ', number2)
+number3 = st.number_input('Insert a third number')
+st.write('The current number is ', number3)
+st.write('The max number is:')
+st.write(max(number1,number2,number3))
+!streamlit run app.py & npx localtunnel --port 8501
